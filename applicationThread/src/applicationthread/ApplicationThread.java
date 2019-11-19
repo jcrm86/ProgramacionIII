5e382df210ee9df585c7b2919a2ee1ee452dd236
jcrm86 <jcrm86@gmail.com>
2019-11-18 21:00:17 -0500

    Se agrega proyecto threads

---
+++ applicationThread/src/applicationthread/ApplicationThread.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applicationthread;
+
+/**
+ *
+ * @author Juan Rivera
+ */
+public class ApplicationThread {
+
+    public static void main(String[] args) {
+         int n = 8; // Numero of threads 
+        for (int i=0; i<8; i++) 
+        { 
+            //Ejercicio 1
+                //ThreadExample object = new ThreadExample(); 
+            //Ejercicio 2
+            Thread object = new Thread(new RunnableThread());
+            object.start(); 
+        } 
+    }
+}
